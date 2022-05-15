dd995f0e043d725ea53b53b2d28a423b6f63da1a
Aleksei <katakuxiko@gmail.com>
2022-05-15 15:33:38 +0600

    delete and update lists

--- pkg/handlers/response.go
+++ pkg/handlers/response.go
@@ -8,8 +8,13 @@ import (
 type errorResponse struct {
 	Message string `json:"message"`
 }
+type statusResponse struct {
+	Status string `json:"status"`
+}
+
 
 func newErrorResponse(c *gin.Context, statusCode int, message string){
 	logrus.Error(message)
 	c.AbortWithStatusJSON(statusCode,errorResponse{message})
 }
+
--- pkg/repository/todo_list_postgress.go
+++ pkg/repository/todo_list_postgress.go
@@ -2,9 +2,11 @@ package repository
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/jmoiron/sqlx"
 	todo "github.com/katakuxiko/rest-api"
+	"github.com/sirupsen/logrus"
 )
 
 type TodoListPostgress struct {
@@ -61,3 +63,71 @@ func (r *TodoListPostgress)GetById(userId,listId int)(todo.TodoList, error){
 	return list, err
 
 }
+func (r *TodoListPostgress)Delete(userId,listId int)error{
+	query := fmt.Sprintf("delete from %s tl USING %s ul WHERE tl.id = ul.list_id AND ul.user_id = $1 AND ul.list_id = $2",
+	todoListTable, userListTable)
+	_, err := r.db.Exec(query, userId, listId)
+	return err
+}
+func (r *TodoListPostgress) Update(userId int, listId int, input todo.UpdateListInput) error{
+	setValues := make([]string, 0)
+	args := make([]interface{}, 0)
+	argId := 1
+
+	if input.Title != nil {
+		setValues = append(setValues, fmt.Sprintf("title=$%d", argId))
+		args = append(args, *input.Title)
+		argId++
+	}
+	if input.Description != nil {
+		setValues = append(setValues, fmt.Sprintf("description=$%d", argId))
+		args = append(args, *input.Description)
+		argId++
+	}
+
+	// title = "update title"
+	// description = "update description"
+	setQuery := strings.Join(setValues, ", ")
+
+	query := fmt.Sprintf("UPDATE %s tl SET %s from %s ul WHERE tl.id = ul.list_id AND ul.list_id=$%d AND ul.user_id=$%d",
+	todoListTable, setQuery, userListTable, argId, argId+1)
+
+	args = append(args, listId, userId)
+	logrus.Debugf("Update query: %s", query)
+	logrus.Debugf("Update query: %s", args)
+
+	_, err := r.db.Exec(query, args...)
+	return err
+}
+// func (r *TodoListPostgress) Update(userId, listId int, input todo.UpdateListInput) error {
+// 	setValues := make([]string, 0)
+// 	args := make([]interface{}, 0)
+// 	argId := 1
+
+// 	if input.Title != nil {
+// 		setValues = append(setValues, fmt.Sprintf("title=$%d", argId))
+// 		args = append(args, *input.Title)
+// 		argId++
+// 	}
+
+// 	if input.Description != nil {
+// 		setValues = append(setValues, fmt.Sprintf("description=$%d", argId))
+// 		args = append(args, *input.Description)
+// 		argId++
+// 	}
+
+// 	// title=$1
+// 	// description=$1
+// 	// title=$1, description=$2
+// 	setQuery := strings.Join(setValues, ", ")
+
+// 	query := fmt.Sprintf("UPDATE %s tl SET %s FROM %s ul WHERE tl.id = ul.list_id AND ul.list_id=$%d AND ul.user_id=$%d",
+// 		todoListTable, setQuery, userListTable, argId, argId+1)
+// 	args = append(args, listId, userId)
+
+// 	logrus.Debugf("updateQuery: %s", query)
+// 	logrus.Debugf("args: %s", args)
+
+// 	_, err := r.db.Exec(query, args...)
+// 	return err
+// }
